f1b6300710cf5584408cafe0922ee0e95887dce1
Soujanya Cheruvu <127056561+soujanyacheruvu@users.noreply.github.com>
2023-10-29 13:46:14 -0400

    Initial Upload

---
+++ src/pages/projects.js
@@ -0,0 +1,245 @@
+import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import proj1 from "public/images/projects/Book Recommendation.png";
+import proj2 from "public/images/projects/Email Spam.png";
+import proj3 from "public/images/projects/Wine.png";
+import proj4 from "public/images/projects/Ads.png";
+import proj5 from "public/images/projects/Whatsapp.png";
+import proj6 from "public/images/projects/Used Cars.png";
+import TransitionEffect from "@/components/TransitionEffect";
+
+const FramerImage = motion(Image);
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+
+  return (
+    <article
+      className="relative flex w-full items-center  justify-between rounded-3xl rounded-br-2xl border
+border-solid border-dark bg-light p-12 shadow-2xl  dark:border-light dark:bg-dark  lg:flex-col 
+lg:p-8 xs:rounded-2xl  xs:rounded-br-3xl xs:p-4 
+    "
+    >
+      <div
+        className="absolute  top-0 -right-3 -z-10 h-[103%] w-[101%] rounded-[2.5rem] rounded-br-3xl bg-dark
+         dark:bg-light  xs:-right-2 xs:h-[102%] xs:w-[100%]
+        xs:rounded-[1.5rem] "
+      />
+
+      <Link
+        href={link}
+        target={"_blank"}
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
+      >
+        <FramerImage
+          src={img}
+          className="h-auto w-full"
+          alt={title}
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          priority
+        />
+      </Link>
+      <div className="flex w-1/2 flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-xl font-medium text-primary dark:text-primaryDark xs:text-base">
+          {type}
+        </span>
+        <Link
+          href={link}
+          target={"_blank"}
+          className="underline-offset-2 hover:underline"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold lg:text-3xl xs:text-2xl">
+            {title}
+          </h2>
+        </Link>
+        <p className=" my-2 rounded-md font-medium text-dark dark:text-light sm:text-sm">
+          {summary}
+        </p>
+        <div className="mt-2 flex items-center">
+          <Link
+            href={github}
+            target={"_blank"}
+            className="w-10"
+            aria-label="Crypto Screener Application github link"
+          >
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target={"_blank"}
+            className="ml-4 rounded-lg
+             bg-dark p-2 px-6 text-lg font-semibold text-light dark:bg-light dark:text-dark 
+             sm:px-4 sm:text-base
+            "
+            aria-label="Crypto Screener Application"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Project = ({ title, type, img, link, github }) => {
+
+  return (
+    <article
+      className="relative flex w-full flex-col items-center justify-center rounded-2xl  rounded-br-2xl 
+      border  border-solid  border-dark bg-light p-6  shadow-2xl dark:border-light dark:bg-dark 
+      xs:p-4
+      "
+    >
+      <div
+        className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%] rounded-[2rem] rounded-br-3xl bg-dark
+         dark:bg-light  md:-right-2 md:w-[101%] xs:h-[102%]
+        xs:rounded-[1.5rem]  "
+      />
+
+      <Link
+        href={link}
+        target={"_blank"}
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="h-auto w-full"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+        />
+      </Link>
+      <div className="mt-4 flex w-full flex-col items-start justify-between">
+        <span className="text-xl font-medium text-primary dark:text-primaryDark lg:text-lg md:text-base">
+          {type}
+        </span>
+
+        <Link
+          href={link}
+          target={"_blank"}
+          className="underline-offset-2 hover:underline"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl ">
+            {title}
+          </h2>
+        </Link>
+        <div className="flex w-full items-center  justify-between">
+          <Link
+            href={link}
+            target={"_blank"}
+            className="rounded text-lg
+            font-medium underline md:text-base
+            "
+            aria-label={title}
+          >
+            Visit
+          </Link>
+          <Link
+            href={github}
+            target={"_blank"}
+            className="w-8 md:w-6"
+            aria-label={title}
+          >
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title> Portfolio | Projects Page</title>
+        <meta
+          name="description"
+          content="Discover the latest projects created by Soujanya"
+        />
+      </Head>
+
+      <TransitionEffect />
+      <main
+        className={`mb-16  flex w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Imagination Trumps Knowledge!"
+            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Featured Project"
+                title="Book Recommendation System"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
+                img={proj1}
+                link="/"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Predictive Analysis"
+                title="House Price Prediction"
+                img={proj2}
+                link="https://github.com/soujanyacheruvu/HousePricePrediction"
+                github="https://github.com/soujanyacheruvu/HousePricePrediction"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Time Series Forecasting, Regression Models"
+                title="Wine Sales Forecast"
+                img={proj3}
+                link="https://github.com/soujanyacheruvu/Time-Series-Analysis"
+                github="https://github.com/soujanyacheruvu/Time-Series-Analysis"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="EDA, Clustering"
+                title="Clustering Ads - Marketing Analytics"
+                summary="Project leverages user preferences and data-driven algorithms to suggest personalized reading choices, 
+                enhancing the reading experience by helping users discover books tailored to their interests."
+                img={proj4}
+                link="https://github.com/soujanyacheruvu/Clustering-Ads"
+                github="https://github.com/soujanyacheruvu/Clustering-Ads"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Natural Language Processing"
+                img={proj5}
+                title="Whatsapp Chat Analysis"
+                link="/"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Principal Component Analysis"
+                img={proj6}
+                title="Home loan transactions - PCA"
+                link="https://github.com/soujanyacheruvu/PCA-Bank-Loan-Transactions/blob/main/PCA%20Home%20Loan%20Transactions%20.ipynb"
+                github="https://github.com/soujanyacheruvu/PCA-Bank-Loan-Transactions/blob/main/PCA%20Home%20Loan%20Transactions%20.ipynb"
+              />
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+}
