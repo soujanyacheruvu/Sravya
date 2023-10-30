1273fcc2a2f98a912f2b2ba1e982017488431713
Sravya Challa <sravyachalla@Sravyas-MacBook-Pro.local>
2023-10-30 14:02:47 +0530

    build fix

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -7,7 +7,7 @@ const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
     <div className="px-2  md:px-10 bg-accent dark:bg-accentDark !text-white dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
       <time className="m-3">
-        {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
+        {blog.publishedAt && format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
       </time>
       {/* <span className="m-3">
         <ViewCounter slug={blogSlug} />
